efec2266a13eb559786dbe2e1db6a9f1837a9394
Marcus Barros <marcusvynicius135@gmail.com>
2025-05-04 23:33:31 -0300

    Usa variável de ambiente PORT para configuração da porta

--- API/backend/index.js
+++ API/backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// Jogos da FURIA no PGL Bucharest 2025
+// A variável de ambiente PORT definida pelo Render será usada
+const port = process.env.PORT || 3000;  // Usa o valor de PORT ou 3000 como fallback
+
 const furiaGames = [
     { team1: "FURIA", team2: "The MongolZ", event: "PGL Bucharest 2025", date: "2025-04-12", result: "0 - 2", type: "finished" },
     { team1: "FURIA", team2: "Virtus.pro", event: "PGL Bucharest 2025", date: "2025-04-13", result: "0 - 2", type: "finished" },
@@ -10,7 +11,6 @@ const furiaGames = [
     { team1: "FURIA", team2: "Betclic", event: "PGL Bucharest 2025", date: "2025-04-14", result: "2 - 0", type: "finished" },
 ];
 
-// Rotas
 app.get('/api/findall', (req, res) => {
     res.json(furiaGames);
 });
